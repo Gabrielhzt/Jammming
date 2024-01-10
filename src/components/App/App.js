047e5e27e4fc6aad08cbb434f331386952227e5b
Gabrielhzt <hazout26@gmail.com>
2024-01-10 10:23:41 +0100

    Added and styled website title

---
+++ src/components/App/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+
+
+  return (
+    <div>
+        <section className='Title'>
+            <h1>Jammming <span className='desc'>Powered by Spotify</span></h1>
+            <h2>Let's find something for your playlist</h2>
+        </section>
+    </div>
+  );
+};
+
+export default App;
