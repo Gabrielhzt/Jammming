83a15fd3482839aa1d19c22d8bf87a4a8fce1c52
Gabrielhzt <hazout26@gmail.com>
2024-01-11 17:27:16 +0100

    Added Spotify API integration

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,12 +1,22 @@
-import React from 'react';
-import './SearchBar.css'
+import React, {useState} from 'react';
+import './SearchBar.css';
 
-const SearchBar = () => {
+const SearchBar = (props) => {
+
+    const [term, setTerm] = useState("");
+
+  function passTerm() {
+    props.onSearch(term);
+  }
+
+  function handleTermChange({ target }) {
+    setTerm(target.value);
+  }
 
     return (
         <div className='bar'>
-            <input className="search" placeholder="Enter A Song Title" />
-            <button>Search</button>
+            <input className="search" placeholder="Enter A Song Title" onChange={handleTermChange} />
+            <button onClick={passTerm}>Search</button>
         </div>
     )
 }
