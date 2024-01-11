ec068a3153dfdfbc9a7955309f5d8fc0f3b44d1d
Gabrielhzt <hazout26@gmail.com>
2024-01-11 17:30:29 +0100

    Added Search reasults

--- src/components/SearchResults/SearchResults.js
+++ src/components/SearchResults/SearchResults.js
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './SearchResults.css'
 import TrackList from '../Tracklist/TrackList';
+import Spotify from '../../util/Spotify';
 
-const SearchResults = () => {
+const SearchResults = (props) => {
+
+    const [term, setTerm] = useState("");
+
+    const [searchResults, setSearchResults] = useState([]);
+
+  const searchSpotify = (term) => {
+    Spotify.search(term).then((results) => {
+      setSearchResults(results);
+    }).catch((error) => {
+      console.error('Error searching Spotify:', error);
+    });
+  };
 
     const result = true;
     return (
         <div className='list'>
-            <TrackList result={result} />
+            <TrackList result={result} userSearchResults={props.userSearchResults} onAdd={props.onAdd} />
         </div>
     )
 };
